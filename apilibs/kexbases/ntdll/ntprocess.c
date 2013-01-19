aadb3af3ee7a7befa75192bb66ace75915dbcdad
Ley0k <leyok@hotmail.fr>
2013-01-19 09:22:00 -0800

    Added NTDLL

---
+++ apilibs/kexbases/ntdll/ntprocess.c
@@ -0,0 +1,119 @@
+/*
+ *  KernelEx
+ *  Copyright (C) 2009, Xeno86
+ *
+ *  This file is part of KernelEx source code.
+ *
+ *  KernelEx is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published
+ *  by the Free Software Foundation; version 2 of the License.
+ *
+ *  KernelEx is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "_ntdll_apilist.h"
+
+NTSTATUS NTAPI ZwSuspendResumeProcess(IN HANDLE ProcessHandle, DWORD dwOperation)
+{
+	DWORD pThread[1024];
+	DWORD cThreads = 0;
+	DWORD dwProcessId = 0;
+	ULONG index = 0;
+	HANDLE hThread = NULL;
+
+	if(dwOperation > 1)
+		return STATUS_INVALID_PARAMETER;
+
+	dwProcessId = GetProcessId_new(ProcessHandle);
+
+	if(dwProcessId == 0)
+		return STATUS_INVALID_PARAMETER;
+
+	kexEnumThreads(dwProcessId, pThread, sizeof(pThread), &cThreads);
+
+	cThreads /= sizeof(DWORD);
+
+	for(index=0;index<=cThreads;index++)
+	{
+		hThread = OpenThread_new(THREAD_SUSPEND_RESUME, FALSE, pThread[index]);
+
+		if(dwOperation == 0)
+		{
+			if(!ResumeThread(hThread))
+				goto _fail;
+		}
+		else if(dwOperation == 1)
+		{
+			if(!SuspendThread(hThread))
+				goto _fail;
+		}
+
+		CloseHandle(hThread);
+	}
+
+	return STATUS_SUCCESS;
+_fail:
+	CloseHandle(hThread);
+	return STATUS_INVALID_PARAMETER;
+}
+
+/* MAKE_EXPORT ZwOpenProcess=NtOpenProcess */
+/* MAKE_EXPORT ZwOpenProcess=ZwOpenProcess */
+NTSTATUS NTAPI ZwOpenProcess(OUT PHANDLE ProcessHandle,
+	IN ACCESS_MASK          AccessMask,
+	IN POBJECT_ATTRIBUTES   ObjectAttributes,
+	IN PCLIENT_ID           ClientId
+)
+{
+	__try
+	{
+		*ProcessHandle = OpenProcess(AccessMask, ObjectAttributes->Attributes, ClientId->UniqueProcessId);
+	}
+	__except(EXCEPTION_EXECUTE_HANDLER)
+	{
+		return STATUS_INVALID_PARAMETER;
+	}
+
+	return STATUS_SUCCESS;
+}
+
+/* MAKE_EXPORT ZwResumeProcess=NtResumeProcess */
+/* MAKE_EXPORT ZwResumeProcess=ZwResumeProcess */
+NTSTATUS NTAPI ZwResumeProcess(IN HANDLE ProcessHandle)
+{
+	return ZwSuspendResumeProcess(ProcessHandle, 0);
+}
+
+/* MAKE_EXPORT ZwSuspendProcess=NtSuspendProcess */
+/* MAKE_EXPORT ZwSuspendProcess=ZwSuspendProcess */
+NTSTATUS NTAPI ZwSuspendProcess(IN HANDLE ProcessHandle)
+{
+	return ZwSuspendResumeProcess(ProcessHandle, 1);
+}
+
+/* MAKE_EXPORT ZwTerminateProcess=NtTerminateProcess */
+/* MAKE_EXPORT ZwTerminateProcess=ZwTerminateProcess */
+NTSTATUS NTAPI ZwTerminateProcess(IN HANDLE ProcessHandle OPTIONAL,
+	IN NTSTATUS ExitStatus
+)
+{
+	BOOL result;
+
+	if(ProcessHandle == NULL)
+		ExitProcess(ExitStatus);
+
+	result = TerminateProcess(ProcessHandle, ExitStatus);
+
+	if(!result)
+		return STATUS_INVALID_PARAMETER;
+
+	return STATUS_SUCCESS;
+}
---
+++ apilibs/kexbases/ntdll/ntthread.c
@@ -0,0 +1,163 @@
+/*
+ *  KernelEx
+ *  Copyright (C) 2009, Xeno86
+ *
+ *  This file is part of KernelEx source code.
+ *
+ *  KernelEx is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published
+ *  by the Free Software Foundation; version 2 of the License.
+ *
+ *  KernelEx is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "_ntdll_apilist.h"
+
+NTSTATUS NTAPI ZwSuspendResumeThread(
+		IN HANDLE ThreadHandle,
+		OUT PULONG PreviousSuspendCount OPTIONAL,
+		IN DWORD dwOperation
+)
+{
+	BOOL result;
+
+	if(dwOperation > 1)
+		return STATUS_INVALID_PARAMETER;
+
+	if(ThreadHandle == NULL)
+		return STATUS_INVALID_PARAMETER;
+
+	if(dwOperation == 0)
+		result = ResumeThread(ThreadHandle);
+	else if(dwOperation == 1)
+		result = SuspendThread(ThreadHandle);
+
+	if(result == -1)
+		return STATUS_INVALID_PARAMETER;
+
+	__try
+	{
+		*PreviousSuspendCount = result;
+	}
+	__except(EXCEPTION_EXECUTE_HANDLER)
+	{
+	}
+
+	return STATUS_SUCCESS;
+}
+
+/* MAKE_EXPORT ZwSetContextThread=NtSetContextThread */
+/* MAKE_EXPORT ZwSetContextThread=ZwSetContextThread */
+NTSTATUS NTAPI ZwSetContextThread(IN HANDLE ThreadHandle,
+	IN PCONTEXT Context
+)
+{
+	BOOL result;
+
+	if(ThreadHandle == NULL || Context == NULL)
+		return STATUS_INVALID_PARAMETER;
+
+	__try
+	{
+		result = SetThreadContext(ThreadHandle, Context);
+	}
+	__except(EXCEPTION_EXECUTE_HANDLER)
+	{
+		return STATUS_INVALID_PARAMETER;
+	}
+
+	if(!result)
+		return STATUS_INVALID_PARAMETER;
+
+	return STATUS_SUCCESS;
+}
+
+/* MAKE_EXPORT ZwGetContextThread=NtGetContextThread */
+/* MAKE_EXPORT ZwGetContextThread=ZwGetContextThread */
+NTSTATUS NTAPI ZwGetContextThread(IN HANDLE ThreadHandle,
+	OUT PCONTEXT pContext
+)
+{
+	BOOL result;
+
+	if(ThreadHandle == NULL || pContext == NULL)
+		return STATUS_INVALID_PARAMETER;
+
+	__try
+	{
+		result = GetThreadContext(ThreadHandle, pContext);
+	}
+	__except(EXCEPTION_EXECUTE_HANDLER)
+	{
+		return STATUS_INVALID_PARAMETER;
+	}
+
+	if(!result)
+		return STATUS_INVALID_PARAMETER;
+
+	return STATUS_SUCCESS;
+}
+
+/* MAKE_EXPORT ZwOpenThread=NtOpenThread */
+/* MAKE_EXPORT ZwOpenThread=ZwOpenThread */
+NTSTATUS NTAPI ZwOpenThread(OUT PHANDLE ThreadHandle,
+	IN ACCESS_MASK          AccessMask,
+	IN POBJECT_ATTRIBUTES   ObjectAttributes,
+	IN PCLIENT_ID           ClientId
+)
+{
+	__try
+	{
+		*ThreadHandle = OpenThread_new(AccessMask, ObjectAttributes->Attributes, ClientId->UniqueProcessId);
+	}
+	__except(EXCEPTION_EXECUTE_HANDLER)
+	{
+		return STATUS_INVALID_PARAMETER;
+	}
+
+	return STATUS_SUCCESS;
+}
+
+/* MAKE_EXPORT ZwResumeThread=NtResumeThread */
+/* MAKE_EXPORT ZwResumeThread=ZwResumeThread */
+NTSTATUS NTAPI ZwResumeThread(
+		IN HANDLE ThreadHandle,
+		OUT PULONG SuspendCount OPTIONAL
+)
+{
+	return ZwSuspendResumeThread(ThreadHandle, SuspendCount, 0);
+}
+
+/* MAKE_EXPORT ZwSuspendThread=NtSuspendThread */
+/* MAKE_EXPORT ZwSuspendThread=ZwSuspendThread */
+NTSTATUS NTAPI ZwSuspendThread(
+		IN HANDLE ThreadHandle,
+		OUT PULONG PreviousSuspendCount OPTIONAL
+)
+{
+	return ZwSuspendResumeThread(ThreadHandle, PreviousSuspendCount, 1);
+}
+
+/* MAKE_EXPORT ZwTerminateThread=NtTerminateThread */
+/* MAKE_EXPORT ZwTerminateThread=ZwTerminateThread */
+NTSTATUS NTAPI ZwTerminateThread(IN HANDLE ThreadHandle,
+	IN NTSTATUS ExitStatus
+)
+{
+	BOOL result;
+
+	result = TerminateThread(ThreadHandle, ExitStatus);
+
+	if(!result)
+		return STATUS_INVALID_PARAMETER;
+
+	return STATUS_SUCCESS;
+}
