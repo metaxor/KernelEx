6afd65f71959635a7f523b6e27b2d4d91f72a834
Ley0k <leyok@hotmail.fr>
2013-01-28 14:42:58 -0800

    added 1 stub

--- auxiliary/netapi32/netapi32.c
+++ auxiliary/netapi32/netapi32.c
@@ -128,3 +128,18 @@ NET_API_STATUS WINAPI NetApiBufferSize(LPVOID Buffer, LPDWORD ByteCount)
 		return NERR_Success;
 	}
 }
+
+/************************************************************
+ *                NetShareEnum  (NETAPI32.@)
+ */
+NET_API_STATUS NET_API_FUNCTION
+NetShareEnum (LMSTR       servername,
+	DWORD   level,
+	LPBYTE  *bufptr,
+	DWORD   prefmaxlen,
+	LPDWORD entriesread,
+	LPDWORD totalentries,
+	LPDWORD resume_handle)
+{
+	return ERROR_CALL_NOT_IMPLEMENTED;
+}
