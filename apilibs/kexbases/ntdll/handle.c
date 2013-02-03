abccd7741a02586ab790747f93a8b4d9fc72de2c
Ley0k <leyok@hotmail.fr>
2013-02-03 14:50:20 -0800

    added handle.c: implemented NtClose

---
+++ apilibs/kexbases/ntdll/handle.c
@@ -0,0 +1,46 @@
+/*
+ *  KernelEx
+ *  Copyright (C) 2013, Ley0k
+ *
+ *  This file is part of KernelEx source code.
+ *
+ *  KernelEx is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published
+ *  by the Free Software Foundation; version 2 of the License.
+ *
+ *  KernelEx is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "_ntdll_apilist.h"
+
+/* MAKE_EXPORT ZwClose=NtClose */
+/* MAKE_EXPORT ZwClose=ZwClose */
+NTSTATUS NTAPI ZwClose(IN HANDLE Handle)
+{
+	BOOL result;
+
+	if(Handle == NULL)
+		return STATUS_INVALID_HANDLE;
+
+	/* Try to close the handle */
+	result = CloseHandle(Handle);
+
+	if(!result)
+	{
+		/* The handle could be a system handle */
+		result = CloseSystemHandle(Handle);
+	}
+
+	if(!result)
+		return GetLastError();
+
+	return STATUS_SUCCESS;
+}
