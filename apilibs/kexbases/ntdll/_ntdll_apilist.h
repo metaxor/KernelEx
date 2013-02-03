9da34f16adccacd3777321c97b360c2a5cb89005
Ley0k <leyok@hotmail.fr>
2013-02-03 14:50:39 -0800

    Implemented NtClose

--- apilibs/kexbases/ntdll/_ntdll_apilist.h
+++ apilibs/kexbases/ntdll/_ntdll_apilist.h
@@ -89,6 +89,7 @@ STUB ZwCreateProcess_stub;
 STUB ZwCreateThread_stub;
 ULONG NTAPI RtlNtStatusToDosError(NTSTATUS Status);
 NTSTATUS NTAPI NtRaiseHardError(IN NTSTATUS ErrorStatus, IN ULONG NumberOfParameters, IN ULONG UnicodeStringParameterMask, IN PULONG_PTR Parameters, IN ULONG ValidResponseOptions, OUT PULONG Response);
+NTSTATUS NTAPI ZwClose(IN HANDLE Handle);
 NTSTATUS NTAPI ZwOpenProcess(OUT PHANDLE ProcessHandle, IN ACCESS_MASK AccessMask, IN POBJECT_ATTRIBUTES ObjectAttributes, IN PCLIENT_ID ClientId);
 NTSTATUS NTAPI ZwResumeProcess(IN HANDLE ProcessHandle);
 NTSTATUS NTAPI ZwSuspendProcess(IN HANDLE ProcessHandle);
