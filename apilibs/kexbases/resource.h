45e00844a91cd7f5c4f96d5fbae38fe92dc05293
Ley0k <leyok@hotmail.fr>
2013-02-09 08:16:20 -0800

    added an end task dialog for logging off

--- apilibs/kexbases/resource.h
+++ apilibs/kexbases/resource.h
@@ -2,14 +2,19 @@
 // Microsoft Developer Studio generated include file.
 // Used by kexbases.rc
 //
+#define IDD_ENDTASK                     101
+#define IDC_WAITTASK                    1000
+#define IDC_ENDTASK                     1001
+#define IDT_STATIC2                     1002
+#define IDT_STATIC1                     65535
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
