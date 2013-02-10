038d255b80c05d4f6583b2ef5cf565db69f7e6bc
Ley0k <leyok@hotmail.fr>
2013-02-10 12:22:27 -0800

    added INITIAL_TEB structure

--- apilibs/kexbases/ntdll/_ntdll_apilist.h
+++ apilibs/kexbases/ntdll/_ntdll_apilist.h
@@ -78,6 +78,14 @@ typedef enum _HARDERROR_RESPONSE
     ResponseContinue
 } HARDERROR_RESPONSE, *PHARDERROR_RESPONSE;
 
+typedef struct _INITIAL_TEB {
+	PVOID StackBase;
+	PVOID StackLimit;
+	PVOID StackCommit;
+	PVOID StackCommitMax;
+	PVOID StackReserved;
+} INITIAL_TEB, *PINITIAL_TEB;
+
 
 BOOL init_ntdll();
 extern const apilib_api_table apitable_ntdll;
@@ -89,14 +97,18 @@ STUB ZwCreateProcess_stub;
 STUB ZwCreateThread_stub;
 ULONG NTAPI RtlNtStatusToDosError(NTSTATUS Status);
 NTSTATUS NTAPI NtRaiseHardError(IN NTSTATUS ErrorStatus, IN ULONG NumberOfParameters, IN ULONG UnicodeStringParameterMask, IN PULONG_PTR Parameters, IN ULONG ValidResponseOptions, OUT PULONG Response);
-NTSTATUS NTAPI ZwDuplicateObject(IN HANDLE SourceProcessHandle, IN HANDLE SourceHandle, IN HANDLE TargetProcessHandle OPTIONAL, OUT PHANDLE TargetHandle OPTIONAL, IN ACCESS_MASK DesiredAccess, IN ULONG HandleAttributes, IN ULONG Options);
 NTSTATUS NTAPI ZwClose(IN HANDLE Handle);
+NTSTATUS NTAPI ZwDuplicateObject(IN HANDLE SourceProcessHandle, IN HANDLE SourceHandle, IN HANDLE TargetProcessHandle OPTIONAL, OUT PHANDLE TargetHandle OPTIONAL, IN ACCESS_MASK DesiredAccess, IN ULONG HandleAttributes, IN ULONG Options);
+NTSTATUS WINAPI ZwWaitForSingleObject(IN HANDLE Handle, IN BOOLEAN Alertable, IN PLARGE_INTEGER Timeout);
+NTSTATUS ZwAllocateVirtualMemory(IN HANDLE ProcessHandle, IN OUT PVOID *BaseAddress, IN ULONG_PTR ZeroBits, IN OUT PSIZE_T RegionSize, IN ULONG AllocationType, IN ULONG Protect);
+NTSTATUS ZwFreeVirtualMemory(IN HANDLE ProcessHandle, IN OUT PVOID *BaseAddress, IN OUT PSIZE_T RegionSize, IN ULONG FreeType);
 NTSTATUS NTAPI ZwOpenProcess(OUT PHANDLE ProcessHandle, IN ACCESS_MASK AccessMask, IN POBJECT_ATTRIBUTES ObjectAttributes, IN PCLIENT_ID ClientId);
 NTSTATUS NTAPI ZwResumeProcess(IN HANDLE ProcessHandle);
 NTSTATUS NTAPI ZwSuspendProcess(IN HANDLE ProcessHandle);
 NTSTATUS NTAPI ZwTerminateProcess(IN HANDLE ProcessHandle OPTIONAL, IN NTSTATUS ExitStatus);
-NTSTATUS NTAPI ZwSetContextThread(IN HANDLE ThreadHandle, IN PCONTEXT Context);
+NTSTATUS NTAPI ZwCreateThread(OUT PHANDLE ThreadHandle, IN ACCESS_MASK DesiredAccess, IN POBJECT_ATTRIBUTES ObjectAttributes OPTIONAL, IN HANDLE ProcessHandle, OUT PCLIENT_ID ClientId, IN PCONTEXT ThreadContext, IN PINITIAL_TEB InitialTeb, IN BOOLEAN CreateSuspended);
 NTSTATUS NTAPI ZwGetContextThread(IN HANDLE ThreadHandle, OUT PCONTEXT pContext);
+NTSTATUS NTAPI ZwSetContextThread(IN HANDLE ThreadHandle, IN PCONTEXT Context);
 NTSTATUS NTAPI ZwOpenThread(OUT PHANDLE ThreadHandle, IN ACCESS_MASK AccessMask, IN POBJECT_ATTRIBUTES ObjectAttributes, IN PCLIENT_ID ClientId);
 NTSTATUS NTAPI ZwResumeThread(IN HANDLE ThreadHandle, OUT PULONG SuspendCount OPTIONAL);
 NTSTATUS NTAPI ZwSuspendThread(IN HANDLE ThreadHandle, OUT PULONG PreviousSuspendCount OPTIONAL);
