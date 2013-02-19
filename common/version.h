c59281244858b59406c3cc0281a62a0fd71ae28f
Ley0k <leyok@hotmail.fr>
2013-02-19 06:44:20 -0800

    updated version

--- common/version.h
+++ common/version.h
@@ -1,30 +1,31 @@
-/*
- *  KernelEx
- *  Copyright (C) 2009-2010, Xeno86
- *
- *  This file is part of KernelEx source code.
- *
- *  KernelEx is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published
- *  by the Free Software Foundation; version 2 of the License.
- *
- *  KernelEx is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with GNU Make; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- */
+//
+//  KernelEx
+//  Copyright (C) 2009-2010, Xeno86
+//
+//  This file is part of KernelEx source code.
+//
+//  KernelEx is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published
+//  by the Free Software Foundation; version 2 of the License.
+//
+//  KernelEx is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with GNU Make; see the file COPYING.  If not, write to
+//  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//
 
 #ifndef __VERSION_H
 #define __VERSION_H
 
-#define VERSION_STR      "4.5.3 Beta 1"
-#define VERSION_CODE     0x04050300
-#define RCVERSION        4, 5, 12, 0
-#define _RCVERSION_      "4, 5, 12, 0"
+#define VERSION_STR      "4.6.0 Beta 1"
+#define VERSION_CODE     0x04060248
+#define VERSION_NUMBER   4;6
+#define RCVERSION        4, 6, 0, 0
+#define _RCVERSION_      "4, 6, 0, 0"
 
 #endif
