3d4feb7d72a2356b46728f122c8549972ae9453e
Ley0k <leyok@hotmail.fr>
2013-03-10 07:00:44 -0700

    Removed stubs for Win98 because WinME's shell32.dll can be used on Win98.

--- apilibs/kexbases/shell32/shmain.c
+++ apilibs/kexbases/shell32/shmain.c
@@ -22,74 +22,7 @@
 #include "_shell32_apilist.h"
 #include <olectl.h>
 
-/* MAKE_EXPORT DllInstall=DllInstall */
-STDAPI DllInstall(BOOL bInstall,
-	LPCWSTR pszCmdLine
-)
+BOOL init_shell32()
 {
-	FIXME("%s %s: stub\n", bInstall ? "TRUE":"FALSE", debugstr_w(pszCmdLine));
-	return S_OK;
-}
-
-/* MAKE_EXPORT DllRegisterServer=DllRegisterServer */
-STDAPI DllRegisterServer(void)
-{
-	DWORD dwVer = kexGetVersion();
-
-	/* Windows ME */
-	if(dwVer == 0xc0005a04)
-	{
-		HINSTANCE hInstance = LoadLibrary("shell32.dll");
-		FARPROC _DllRegisterServer = NULL;
-		HRESULT Result = 0;
-
-		if(hInstance == NULL)
-			return E_NOTIMPL;
-
-		_DllRegisterServer = GetProcAddress(hInstance, "DllRegisterServer");
-
-		if(_DllRegisterServer == NULL)
-			return E_NOTIMPL;
-
-		Result = _DllRegisterServer();
-
-		FreeLibrary(hInstance);
-
-		return Result;
-	}
-
-	FIXME_OUT("Update the registry from resource\n");
-	FIXME_OUT("Register shell folders\n");
-	return S_OK;
-}
-
-/* MAKE_EXPORT DllUnregisterServer=DllUnregisterServer */
-STDAPI DllUnregisterServer(void)
-{
-	DWORD dwVer = kexGetVersion();
-
-	/* Windows ME */
-	if(dwVer == 0xc0005a04)
-	{
-		HINSTANCE hInstance = LoadLibrary("shell32.dll");
-		FARPROC _DllUnregisterServer = NULL;
-		HRESULT Result = 0;
-
-		if(hInstance == NULL)
-			return E_NOTIMPL;
-
-		_DllUnregisterServer = GetProcAddress(hInstance, "DllUnregisterServer");
-
-		if(_DllUnregisterServer == NULL)
-			return E_NOTIMPL;
-
-		Result = _DllUnregisterServer();
-
-		FreeLibrary(hInstance);
-
-		return Result;
-	}
-
-	FIXME_OUT("Update the registry from resource\n");
-	return S_OK;
+	return TRUE;
 }
