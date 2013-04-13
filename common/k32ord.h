9c5e5d10626c13eab1d1990c2b899fe799e6b543
Ley0k <leyok@hotmail.fr>
2013-04-13 14:42:31 -0700

    added more ordinals

--- common/k32ord.h
+++ common/k32ord.h
@@ -53,14 +53,15 @@ MAKE_HEADER(void __stdcall _RestoreThunkLock(DWORD MutexCount))
 MAKE_HEADER(void __stdcall _LeaveSysLevel(CRITICAL_SECTION* cs))
 MAKE_HEADER(void __stdcall _LeaveSysLevel_NoThk(CRITICAL_SECTION* cs))
 
-MAKE_HEADER(ULONG __stdcall VxDCall1(ULONG p1))
-MAKE_HEADER(ULONG __stdcall VxDCall2(ULONG p1, ULONG p2))
-MAKE_HEADER(ULONG __stdcall VxDCall3(ULONG p1, ULONG p2, ULONG p3))
-MAKE_HEADER(ULONG __stdcall VxDCall4(ULONG p1, ULONG p2, ULONG p3, ULONG p4))
-MAKE_HEADER(ULONG __stdcall VxDCall5(ULONG p1, ULONG p2, ULONG p3, ULONG p4, ULONG p5))
-MAKE_HEADER(ULONG __stdcall VxDCall6(ULONG p1, ULONG p2, ULONG p3, ULONG p4, ULONG p5, ULONG p6))
-MAKE_HEADER(ULONG __stdcall VxDCall7(ULONG p1, ULONG p2, ULONG p3, ULONG p4, ULONG p5, ULONG p6, ULONG p7))
-MAKE_HEADER(ULONG __stdcall VxDCall8(ULONG p1, ULONG p2, ULONG p3, ULONG p4, ULONG p5, ULONG p6, ULONG p7, ULONG p8))
+MAKE_HEADER(ULONG __stdcall VxDCall0(DWORD service))
+MAKE_HEADER(ULONG __stdcall VxDCall1(DWORD service, ULONG p1))
+MAKE_HEADER(ULONG __stdcall VxDCall2(DWORD service, ULONG p1, ULONG p2))
+MAKE_HEADER(ULONG __stdcall VxDCall3(DWORD service, ULONG p1, ULONG p2, ULONG p3))
+MAKE_HEADER(ULONG __stdcall VxDCall4(DWORD service, ULONG p1, ULONG p2, ULONG p3, ULONG p4))
+MAKE_HEADER(ULONG __stdcall VxDCall5(DWORD service, ULONG p1, ULONG p2, ULONG p3, ULONG p4, ULONG p5))
+MAKE_HEADER(ULONG __stdcall VxDCall6(DWORD service, ULONG p1, ULONG p2, ULONG p3, ULONG p4, ULONG p5, ULONG p6))
+MAKE_HEADER(ULONG __stdcall VxDCall7(DWORD service, ULONG p1, ULONG p2, ULONG p3, ULONG p4, ULONG p5, ULONG p6, ULONG p7))
+MAKE_HEADER(ULONG __stdcall VxDCall8(DWORD service, ULONG p1, ULONG p2, ULONG p3, ULONG p4, ULONG p5, ULONG p6, ULONG p7, ULONG p8))
 
 MAKE_HEADER(void __stdcall MakeCriticalSectionGlobal(CRITICAL_SECTION* cs))
 MAKE_HEADER(HANDLE __stdcall ConvertToGlobalHandle(HANDLE hObject))
@@ -70,12 +71,27 @@ MAKE_HEADER(BOOL __stdcall RegisterServiceProcess(DWORD dwProcessId, BOOL fRegis
 
 MAKE_HEADER(ULONG __stdcall CommonUnimpStub())
 
+MAKE_HEADER(WORD __stdcall GlobalAlloc16(UINT uFlags, SIZE_T dwBytes))
+MAKE_HEADER(LPVOID __stdcall GlobalLock16(HGLOBAL hMem))
+MAKE_HEADER(SIZE_T __stdcall GlobalSize16(HGLOBAL hMem))
+MAKE_HEADER(BOOL __stdcall GlobalUnlock16(HGLOBAL hMem))
+
+MAKE_HEADER(LPVOID __stdcall K32WOWGetVDMPointerFix(DWORD vp1616, DWORD dwBytes, BOOL fProtect))
+MAKE_HEADER(void __stdcall K32WOWGetVDMPointerUnfix(DWORD vp1616))
+MAKE_HEADER(WORD __stdcall MapHModuleLS(HMODULE hModule))
 MAKE_HEADER(DWORD __stdcall MapSL(DWORD addr16))
+MAKE_HEADER(DWORD __stdcall MapLS(DWORD addr32))
+
 MAKE_HEADER(DWORD __stdcall LoadLibrary16(LPSTR libname))
 MAKE_HEADER(DWORD __stdcall GetProcAddress16(HANDLE hModule, LPSTR procname))
-MAKE_HEADER(void __cdecl  QT_Thunk())
 MAKE_HEADER(DWORD __stdcall FreeLibrary16(HANDLE hModule))
+
+MAKE_HEADER(void __cdecl  QT_Thunk())
 MAKE_HEADER(ULONG __stdcall K32_RtlNtStatusToDosError(LONG status))
+
+MAKE_HEADER(void __stdcall GDIReallyCares(HMODULE hModule))
+
+MAKE_HEADER(BOOL __stdcall ThunkConnect32(PVOID thunkdata, LPSTR thunkname, LPSTR dll16, LPSTR dll32, HINSTANCE hInst, DWORD dwReason))
 
 #ifdef __cplusplus
 }
