84f2312427e65e9223eccb63bb1ed55e1f1d0129
Ley0k <leyok@hotmail.fr>
2013-04-15 17:02:31 -0700

    implemented NtCreateSection nearly implemented NtQuerySection

--- apilibs/kexbases/ntdll/ntfile.c
+++ apilibs/kexbases/ntdll/ntfile.c
@@ -21,14 +21,128 @@
 
 #include "_ntdll_apilist.h"
 
+/* MAKE_EXPORT NtCreateSection=NtCreateSection */
+/* MAKE_EXPORT NtCreateSection=ZwCreateSection */
+NTSTATUS
+NTAPI
+NtCreateSection(
+	OUT PHANDLE SectionHandle,
+	IN ACCESS_MASK DesiredAccess,
+	IN POBJECT_ATTRIBUTES ObjectAttributes OPTIONAL,
+	IN PLARGE_INTEGER MaximumSize OPTIONAL,
+	IN ULONG SectionPageProtection OPTIONAL,
+	IN ULONG AllocationAttributes,
+	IN HANDLE FileHandle OPTIONAL
+)
+{
+	HANDLE hFileMapping, hGlobalHandle;
+	SECURITY_ATTRIBUTES sa;
+	BOOL fInherit, fGlobal;
+	DWORD dwMaximumSizeLow, dwMaximumSizeHigh;
+
+	if(FileHandle == NULL)
+		FileHandle = INVALID_HANDLE_VALUE;
+
+    /* Check for non-existing flags */
+    if ((AllocationAttributes & ~(SEC_COMMIT | SEC_RESERVE |
+								SEC_LARGE_PAGES | SEC_IMAGE | SEC_NOCACHE)))
+	{
+		if(!(AllocationAttributes & 1))
+		{
+			ERR("Bogus allocation attribute: %lx\n", AllocationAttributes);
+			return STATUS_INVALID_PARAMETER_6;
+		}
+	}
+
+	/* Check for no allocation type */
+	if (!(AllocationAttributes & (SEC_COMMIT | SEC_RESERVE | SEC_IMAGE)))
+	{
+		ERR_OUT("Missing allocation type in allocation attributes\n");
+		return STATUS_INVALID_PARAMETER_6;
+	}
+
+    /* Check for image allocation with invalid attributes */
+    if((AllocationAttributes & SEC_IMAGE) &&
+		(AllocationAttributes & (SEC_COMMIT | SEC_RESERVE | SEC_LARGE_PAGES |
+								SEC_NOCACHE)))
+	{
+		ERR_OUT("Image allocation with invalid attributes\n");
+		return STATUS_INVALID_PARAMETER_6;
+	}
+
+	/* Check for allocation type is both commit and reserve */
+	if((AllocationAttributes & SEC_COMMIT) && (AllocationAttributes & SEC_RESERVE))
+	{
+		ERR_OUT("Commit and reserve in the same time\n");
+		return STATUS_INVALID_PARAMETER_6;
+	}
+
+	/* Now check for valid protection */
+	if((SectionPageProtection & PAGE_NOCACHE) ||
+		(SectionPageProtection & PAGE_WRITECOMBINE) ||
+		(SectionPageProtection & PAGE_GUARD) ||
+		(SectionPageProtection & PAGE_NOACCESS))
+	{
+		ERR_OUT("Sections don't support these protections\n");
+		return STATUS_INVALID_PAGE_PROTECTION;
+	}
+
+	if(IsBadWritePtr(SectionHandle, sizeof(DWORD)))
+		return STATUS_INVALID_PARAMETER_1;
+
+	if(ObjectAttributes != NULL)
+	{
+		fInherit = (ObjectAttributes->Attributes & OBJ_INHERIT) ? TRUE : FALSE;
+		fGlobal = (ObjectAttributes->Attributes & OBJ_KERNEL_HANDLE) ? TRUE : FALSE;
+		sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+		sa.lpSecurityDescriptor = NULL;
+		sa.bInheritHandle = fInherit;
+	}
+	else
+	{
+		fInherit = FALSE;
+		fGlobal = FALSE;
+	}
+
+	if(MaximumSize != NULL)
+	{
+		dwMaximumSizeLow = MaximumSize->LowPart;
+		dwMaximumSizeHigh = MaximumSize->HighPart;
+	}
+
+	hFileMapping = CreateFileMapping(FileHandle, fInherit ? &sa : NULL,
+									SectionPageProtection | AllocationAttributes,
+									dwMaximumSizeHigh,
+									dwMaximumSizeLow,
+									NULL);
+
+	if(hFileMapping == NULL)
+		return GetLastError(); // FIXME: Return a status code
+
+	/* If this is a global handle, return the global one and close the other handle */
+	if(fGlobal)
+	{
+		CloseHandle(hFileMapping);
+		hGlobalHandle = ConvertToGlobalHandle(hFileMapping);
+		*SectionHandle = hGlobalHandle;
+	}
+	else
+		*SectionHandle = hFileMapping;
+
+	return STATUS_SUCCESS;
+}
+
 /* MAKE_EXPORT NtOpenFile=NtOpenFile */
-NTSTATUS NTAPI NtOpenFile(
-  OUT  PHANDLE FileHandle,
-  IN   ACCESS_MASK DesiredAccess,
-  IN   POBJECT_ATTRIBUTES ObjectAttributes,
-  OUT  PIO_STATUS_BLOCK IoStatusBlock,
-  IN   ULONG ShareAccess,
-  IN   ULONG OpenOptions
+/* MAKE_EXPORT NtOpenFile=ZwOpenFile */
+NTSTATUS
+NTAPI
+NtOpenFile(
+	OUT PHANDLE FileHandle,
+	IN ACCESS_MASK DesiredAccess,
+	IN POBJECT_ATTRIBUTES ObjectAttributes,
+	OUT PIO_STATUS_BLOCK IoStatusBlock,
+	IN ULONG ShareAccess,
+	IN ULONG OpenOptions
 )
 {
 	PCHAR lpFileName;
@@ -81,6 +195,81 @@ NTSTATUS NTAPI NtOpenFile(
 			NtClose(*FileHandle);
 			return STATUS_ACCESS_DENIED; // what to return in case of this fail ?
 		}
+	}
+
+	return STATUS_SUCCESS;
+}
+
+/* MAKE_EXPORT NtQuerySection=NtQuerySection */
+/* MAKE_EXPORT NtQuerySection=ZwQuerySection */
+NTSTATUS
+NTAPI
+NtQuerySection(
+	IN HANDLE SectionHandle,
+	IN SECTION_INFORMATION_CLASS SectionInformationClass,
+	OUT PVOID SectionInformation,
+	IN SIZE_T SectionInformationLength,
+	OUT PSIZE_T ResultLength OPTIONAL
+)
+{
+	PSECTION_BASIC_INFORMATION pSectionBasicInfo;
+	PSECTION_IMAGE_INFORMATION pSectionImageInfo;
+	LPVOID lpFileBase;
+	PIMAGE_DOS_HEADER dosHeader;
+	PIMAGE_NT_HEADERS pNTHeader;
+	PIMAGE_OPTIONAL_HEADER32 pOptHeader;
+	PIMAGE_FILE_HEADER pFileHeader;
+
+	if(SectionHandle == NULL)
+		return STATUS_INVALID_PARAMETER_1;
+
+	if(SectionInformationLength != sizeof(SECTION_BASIC_INFORMATION) &&
+		SectionInformationLength != sizeof(SECTION_IMAGE_INFORMATION))
+		return STATUS_INVALID_PARAMETER;
+		
+	if(IsBadWritePtr(SectionInformation, SectionInformationLength))
+		return STATUS_INVALID_PARAMETER_3;
+
+	pSectionBasicInfo = (PSECTION_BASIC_INFORMATION)SectionInformation;
+	pSectionImageInfo = (PSECTION_IMAGE_INFORMATION)SectionInformation;
+
+	lpFileBase = MapViewOfFile(SectionHandle,
+							FILE_MAP_READ,
+							0,
+							0,
+							0);
+
+	if(lpFileBase == NULL)
+		return STATUS_INVALID_PARAMETER; // FIXME: Return other values
+
+	dosHeader = (PIMAGE_DOS_HEADER)lpFileBase;
+
+	__try
+	{
+		pNTHeader = (PIMAGE_NT_HEADERS)dosHeader->e_lfanew;
+		pOptHeader = &pNTHeader->OptionalHeader;
+		pFileHeader = &pNTHeader->FileHeader;
+
+		/* FIXME: Now copy the remaining data to the SECTION_BASIC_INFORMATION/SECTION_IMAGE_INFORMATION
+		   structures */
+
+		pSectionBasicInfo->BaseAddress = lpFileBase;
+
+		if(SectionInformationLength == sizeof(SECTION_IMAGE_INFORMATION))
+		{
+			pSectionImageInfo->DllCharacteristics = pOptHeader->DllCharacteristics;
+			pSectionImageInfo->ImageCharacteristics = pFileHeader->Characteristics;
+			pSectionImageInfo->Machine = pFileHeader->Machine;
+			pSectionImageInfo->SubSystemMajorVersion = pOptHeader->MajorSubsystemVersion;
+			pSectionImageInfo->SubSystemMinorVersion = pOptHeader->MinorSubsystemVersion;
+			pSectionImageInfo->SubSystemType = pOptHeader->Subsystem;
+		}
+		else
+		{
+		}
+	}
+	__except(EXCEPTION_EXECUTE_HANDLER)
+	{
 	}
 
 	return STATUS_SUCCESS;
