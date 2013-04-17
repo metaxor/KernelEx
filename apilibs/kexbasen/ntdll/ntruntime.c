1f414bfaa118a905ca8f485cd6f4330d84509af0
Ley0k <leyok@hotmail.fr>
2013-04-16 17:40:11 -0700

    added atoi, atol and _chkstk

--- apilibs/kexbasen/ntdll/ntruntime.c
+++ apilibs/kexbasen/ntdll/ntruntime.c
@@ -21,7 +21,101 @@
 
 #include "_ntdll_apilist.h"
 
+static inline int digitval(int ch)
+{
+	if (ch >= '0' && ch <= '9') {
+		return ch - '0';
+	} else if (ch >= 'A' && ch <= 'Z') {
+		return ch - 'A' + 10;
+	} else if (ch >= 'a' && ch <= 'z') {
+		return ch - 'a' + 10;
+	} else {
+		return -1;
+	}
+}
+
+uintmax_t strntoumax(const char *nptr, char **endptr, int base, size_t n)
+{
+	int minus = 0;
+	uintmax_t v = 0;
+	int d;
+
+	while (n && isspace((unsigned char)*nptr)) {
+		nptr++;
+		n--;
+	}
+
+	/* Single optional + or - */
+	if (n) {
+		char c = *nptr;
+		if (c == '-' || c == '+') {
+			minus = (c == '-');
+			nptr++;
+			n--;
+		}
+	}
+
+	if (base == 0) {
+		if (n >= 2 && nptr[0] == '0' &&
+		    (nptr[1] == 'x' || nptr[1] == 'X')) {
+			n -= 2;
+			nptr += 2;
+			base = 16;
+		} else if (n >= 1 && nptr[0] == '0') {
+			n--;
+			nptr++;
+			base = 8;
+		} else {
+			base = 10;
+		}
+	} else if (base == 16) {
+		if (n >= 2 && nptr[0] == '0' &&
+		    (nptr[1] == 'x' || nptr[1] == 'X')) {
+			n -= 2;
+			nptr += 2;
+		}
+	}
+
+	while (n && (d = digitval(*nptr)) >= 0 && d < base) {
+		v = v * base + d;
+		n--;
+		nptr++;
+	}
+
+	if (endptr)
+		*endptr = (char *)nptr;
+
+	return minus ? -(long)v : v;
+}
+
 /* MAKE_EXPORT _atoi64=_atoi64 */
+/* MAKE_EXPORT _chkstk=_chkstk */
+__declspec(naked) void _chkstk(void)
+{
+__asm {
+		push    ecx
+		cmp     eax, 1000h
+		lea     ecx, [esp+4+4]
+		jb      short _below
+
+_loop:
+		sub     ecx, 1000h
+		sub     eax, 1000h
+		test    [ecx], eax
+		cmp     eax, 1000h
+		jnb     short _loop
+
+_below:
+		sub     ecx, eax
+		mov     eax, esp
+		test    [ecx], eax
+		mov     esp, ecx
+		mov     ecx, [eax]
+		mov     eax, [eax+4]
+		push    eax
+		retn
+	}
+}
 /* MAKE_EXPORT _itoa=_itoa */
 /* MAKE_EXPORT _itow=_itow */
 /* MAKE_EXPORT _ltoa=_ltoa */
@@ -49,6 +143,18 @@
 /* MAKE_EXPORT _wtol=_wtol */
 /* MAKE_EXPORT abs=abs */
 /* MAKE_EXPORT atan=atan */
+/* MAKE_EXPORT atoi_NT=atoi */
+int atoi_NT(const char *nptr)
+{
+	return (int)strntoumax(nptr, (char **)NULL, 10, ~(size_t) 0);
+}
+
+/* MAKE_EXPORT atol_NT=atol */
+int atol_NT(const char *nptr)
+{
+	return (long)strntoumax(nptr, (char **)NULL, 10, ~(size_t) 0);
+}
+
 /* MAKE_EXPORT ceil=ceil */
 /* MAKE_EXPORT cos=cos */
 /* MAKE_EXPORT fabs=fabs */
