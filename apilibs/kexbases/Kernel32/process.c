226d644111c0d6b6b17cd3c22c9803e35f2f2e89
Ley0k <leyok@hotmail.fr>
2013-04-16 17:45:03 -0700

    Checking for file format now fully working

--- apilibs/kexbases/Kernel32/process.c
+++ apilibs/kexbases/Kernel32/process.c
@@ -27,15 +27,17 @@
 /* MAKE_EXPORT CreateProcessA_fix=CreateProcessA */
 BOOL WINAPI CreateProcessA_fix(LPCSTR lpApplicationName, LPSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes, LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles, DWORD dwCreationFlags, LPVOID lpEnvironment, LPCSTR lpCurrentDirectory, LPSTARTUPINFOA lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation)
 {
-    PTDB98 Thread = get_tdb();
-	PPDB98 Process = get_pdb();
-	PCHAR pszDesktop = NULL;
-	BOOL result = FALSE;
-	HANDLE hFile;
-	HANDLE hFileMapping;
-	LPVOID lpFileBase;
-	PIMAGE_DOS_HEADER dosHeader;
-	PIMAGE_NT_HEADERS pNTHeader;
+    PTDB98				Thread = get_tdb();
+	PPDB98				Process = get_pdb();
+	PCHAR				pszDesktop = NULL;
+	ULONG				result = 0;
+	LPSTR				lpString, lpNameBuffer, lpNullBuffer;
+	HANDLE				hFile;
+	HANDLE				hFileMapping;
+	LPVOID				lpFileBase;
+	PIMAGE_DOS_HEADER	dosHeader;
+	PIMAGE_NT_HEADERS	pNTHeader;
+	BOOL				fCmdLineApp;
 
     if(IsBadWritePtr(lpStartupInfo, sizeof(STARTUPINFO)) || IsBadWritePtr(lpProcessInformation, sizeof(PROCESS_INFORMATION)))
         return FALSE;
@@ -59,17 +61,166 @@ BOOL WINAPI CreateProcessA_fix(LPCSTR lpApplicationName, LPSTR lpCommandLine, LP
     if(dwCreationFlags & CREATE_UNICODE_ENVIRONMENT)
         dwCreationFlags &= ~CREATE_UNICODE_ENVIRONMENT;
 
-	hFile = CreateFileA_fix(lpApplicationName != NULL ? lpApplicationName : lpCommandLine,
+	if(lpApplicationName == NULL)
+	{
+		BOOL fSearchDone = FALSE;
+		BOOL fFoundQuotes = FALSE;
+		BOOL fQuotesNeeded = FALSE;
+		CHAR cTemp;
+
+		lpNameBuffer = (LPSTR)HeapAlloc(GetProcessHeap(),
+										0,
+										MAX_PATH * sizeof(CHAR));
+
+		if(lpNameBuffer == NULL)
+		{
+			SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+			goto Cleanup;
+		}
+
+		/* This is all we have to work with */
+		lpApplicationName = lpCommandLine;
+
+		/* Initialize our friends at the beginning */
+		lpNullBuffer = (LPSTR)lpApplicationName;
+		lpString = (LPSTR)lpApplicationName;
+
+		/* We will start by looking for a quote */
+		if(*lpString == L'\"')
+		{
+			/* That was quick */
+			fSearchDone = TRUE;
+
+			/* Advance past quote */
+			lpString++;
+			lpApplicationName = lpString;
+
+             /* Find the closing quote */
+			while(*lpString)
+			{
+				if (*lpString == L'\"' && *(lpString - 1) != L'^')
+				{
+					/* Found it */
+					lpNullBuffer = lpString;
+					fFoundQuotes = TRUE;
+					break;
+				}
+
+				/* Keep looking */
+				lpString++;
+				lpNullBuffer = lpString;
+			}
+		}
+		else
+		{
+			/* No quotes, so we'll be looking for white space */
+WhiteScan:
+			/* Reset the pointer */
+			lpApplicationName = lpCommandLine;
+
+			/* Find whitespace of Tab */
+			while(*lpString)
+			{
+				if(*lpString == ' ' || *lpString == '\t')
+				{
+					/* Found it */
+					lpNullBuffer = lpString;
+					break;
+				}
+
+				/* Keep looking */
+				lpString++;
+				lpNullBuffer = lpString;
+			}
+		}
+
+		/* Set the Null Buffer */
+		cTemp = *lpNullBuffer;
+		*lpNullBuffer = UNICODE_NULL;
+
+		TRACE("Ready for SearchPath: %S\n", lpApplicationName);
+		/* Do a search for the file */
+		result = SearchPath(NULL,
+						lpApplicationName,
+						".exe",
+						MAX_PATH,
+						lpNameBuffer,
+						NULL) * sizeof(CHAR);
+
+		/* Did it find something? */
+		if(result)
+		{
+			/* Get file attributes */
+			ULONG Attributes = GetFileAttributes(lpNameBuffer);
+			if (Attributes & FILE_ATTRIBUTE_DIRECTORY)
+			{
+				/* Give it a length of 0 to fail, this was a directory. */
+				result = FALSE;
+			}
+			else
+			{
+				/* It's a file! */
+				result += sizeof(WCHAR);
+			}
+		}
+
+		/* Now check if we have a file, and if the path size is OK */
+		if(!result || result >= (MAX_PATH * sizeof(WCHAR)))
+		{
+			/* Put back the command line */
+			*lpNullBuffer = cTemp;
+			lpApplicationName = lpNameBuffer;
+
+			TRACE("SearchPath failed. result: %ld\n", result);
+
+		   /*
+			* If the search isn't done and we still have cmdline
+			* then start over. Ex: c:\ha ha ha\haha.exe
+			*/
+			if(*lpString && !fSearchDone)
+			{
+				/* Move in the buffer */
+				lpString++;
+				lpNullBuffer = lpString;
+
+				/* We will have to add a quote, since there is a space*/
+				fQuotesNeeded = TRUE;
+
+				/* And we will also fake the fact we found one */
+				fFoundQuotes = TRUE;
+
+				/* Start over */
+				goto WhiteScan;
+			}
+
+			/* We totally failed */
+			goto Cleanup;
+		}
+
+		/* Put back the command line */
+		*lpNullBuffer = cTemp;
+		lpApplicationName = lpNameBuffer;
+		TRACE("SearchPath suceeded (%ld): ", result);
+		DBGPRINTF(("%S\n", lpNameBuffer));
+	}
+	else if(lpCommandLine == NULL || *lpCommandLine == ANSI_NULL)
+	{
+		/* We have an app name (good!) but no command line */
+		fCmdLineApp = TRUE;
+		lpCommandLine = (LPSTR)lpApplicationName;
+	}
+
+	hFile = CreateFileA_fix(lpApplicationName,
 							GENERIC_READ,
 							FILE_SHARE_READ,
 							NULL,
 							OPEN_EXISTING,
 							FILE_ATTRIBUTE_NORMAL,
 							0);
-                    
+
 	if(hFile == INVALID_HANDLE_VALUE)
 	{
-		SetLastError(ERROR_FILE_NOT_FOUND);
+		TRACE("File not found ! (target=%s, err=%d)\n", lpApplicationName, GetLastError());
 		return FALSE;
 	}
 
@@ -82,7 +233,7 @@ BOOL WINAPI CreateProcessA_fix(LPCSTR lpApplicationName, LPSTR lpCommandLine, LP
 
 	if(hFileMapping == NULL)
 	{
-		SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+		TRACE("Failed to create a mapping of the file ! (err=%d)\n", GetLastError());
 		return FALSE;
 	}
 
@@ -94,7 +245,7 @@ BOOL WINAPI CreateProcessA_fix(LPCSTR lpApplicationName, LPSTR lpCommandLine, LP
 
 	if(lpFileBase == NULL)
 	{
-		SetLastError(ERROR_NOT_ENOUGH_MEMORY);
+		TRACE("Failed to map view of the file ! (err=%d)", GetLastError());
 		return FALSE;
 	}
 
@@ -102,27 +253,37 @@ BOOL WINAPI CreateProcessA_fix(LPCSTR lpApplicationName, LPSTR lpCommandLine, LP
 
 	__try
 	{
-		pNTHeader = (PIMAGE_NT_HEADERS)dosHeader->e_lfanew;
+		pNTHeader = (PIMAGE_NT_HEADERS)((DWORD)lpFileBase + dosHeader->e_lfanew);
 
+		/* Fail if the executable is a DLL (Win9x doesn't care if the image
+		   is a DLL but can execute) */
 		if(pNTHeader->FileHeader.Characteristics == IMAGE_FILE_DLL)
 		{
+			TRACE("Executable %s is a DLL !\n", lpApplicationName);
 			SetLastError(ERROR_BAD_EXE_FORMAT);
 			return FALSE;
 		}
 
+		/* The executable must be a GUI/CUI (Win9x can execute other subsystem anyway) */
 		if(pNTHeader->OptionalHeader.Subsystem != IMAGE_SUBSYSTEM_WINDOWS_GUI &&
 			pNTHeader->OptionalHeader.Subsystem != IMAGE_SUBSYSTEM_WINDOWS_CUI)
 		{
 			/* Set the famous "The %1 application cannot be run in Win32 mode" error */
+			TRACE("Executable %s cannot be run under Win32 mode\n", lpApplicationName);
 			SetLastError(ERROR_CHILD_NOT_COMPLETE);
 			return FALSE;
 		}
 	}
 	__except(EXCEPTION_EXECUTE_HANDLER)
 	{
+		TRACE("An exception occured, the executable %s is in a bad format\n", lpApplicationName);
 		SetLastError(ERROR_BAD_EXE_FORMAT);
 		return FALSE;
 	}
+
+	UnmapViewOfFile(lpFileBase);
+	CloseHandle(hFileMapping);
+	CloseHandle(hFile);
 
 	__try
 	{
@@ -153,13 +314,16 @@ BOOL WINAPI CreateProcessA_fix(LPCSTR lpApplicationName, LPSTR lpCommandLine, LP
 						 lpStartupInfo,
 						 lpProcessInformation);
 
+Cleanup:
 	if(!result)
-	{
 		free(pszDesktop);
-		return FALSE;
-	}
 
-	return TRUE;
+	if(lpNameBuffer != NULL)
+		HeapFree(GetProcessHeap(),
+				0,
+				lpNameBuffer);
+
+	return result;
 }
 
 /* MAKE_EXPORT CreateProcessAsUserA_new=CreateProcessAsUserA*/
