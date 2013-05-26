e1a6d3a4f04bb30885dd5b563e52594a6d51e0a4
Ley0k <leyok@hotmail.fr>
2013-05-26 08:43:43 -0700

    added userheap.c (not sure if it will be useful)

---
+++ apilibs/kexbases/User32/userheap.c
@@ -0,0 +1,23 @@
+/*
+ *  KernelEx
+ *  Copyright (C) 2013, Ley0k
+ *
+ *  This file is part of KernelEx source code.
+ *
+ *  KernelEx is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published
+ *  by the Free Software Foundation; version 2 of the License.
+ *
+ *  KernelEx is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include "common.h"
+
